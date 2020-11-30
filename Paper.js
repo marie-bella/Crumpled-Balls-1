d5870d85376fc88578701d9592accea9b761630f
marie-bella <71880657+marie-bella@users.noreply.github.com>
2020-11-30 17:10:09 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper{
+
+    constructor(x,y,radius){
+
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+
+        this.radius= radius;
+        this.body= Bodies.circle(x,y,radius,options);
+        World.add(world,this.body);
+
+    }
+    display(){
+        var pos = this.body.position;
+
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        strokeWeight(4);
+        fill(200,0,200);
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+    }
+}
